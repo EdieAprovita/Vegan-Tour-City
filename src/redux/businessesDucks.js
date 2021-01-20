c461adfc7e0955f8143eabffdec1967e32eaf407
EdieAprovita <edieveg316@gmail.com>
2021-01-20 12:38:23 -0600

    Adding business ducks

--- src/redux/businessesDucks.js
+++ src/redux/businessesDucks.js
@@ -7,22 +7,42 @@ import { baseURLBusiness } from '../services/apiServices'
 const initialData = {
 	businessArr: [],
 	status: '',
-	error:undefined,
+	error: undefined,
 }
 
 //Types
+const LOADING = 'LOADING'
+
 const GET_ALL_BUSINESSES = 'GET_ALL_BUSINESSES'
-const GET_BUSINESS = 'GET_BUSINESS'
+const GET_ALL_BUSINESSES_ERROR = 'GET_ALL_BUSINESSES_ERROR'
+
+const GET_ALL_BUSINESS = 'GET_ALL_BUSINESS'
+const GET_BUSINESS_ERROR = 'GET_BUSINESS_ERROR'
+
 const CREATE_BUSINESS = 'CREATE_BUSINESS'
+const CREATE_BUSINESS_ERROR = 'CREATE_BUSINESS_ERROR'
+
 const UPDATE_BUSINESS = 'UPDATE_BUSINESS'
+const UPDATE_BUSINESS_ERROR = 'UPDATE_BUSINESS_ERROR'
+
 const DELETE_BUSINESS = 'DELETE_BUSINESS'
+const DELETE_BUSINESS_ERROR = 'DELETE_BUSINESS_ERROR'
+
+const GETTOPBUSINESS = 'GETTOPBUSINESS'
+const GETTOPBUSINESS_ERROR = 'GETTOPBUSINESS_ERROR'
+
+const CREATEBUSINESSREVIEW = 'CREATEBUSINESSREVIEW'
+const CREATEBUSINESSREVIEW_ERROR = 'CREATEBUSINESSREVIEW_ERROR'
 
 //Reducer
 
 export default function businessReducer(state = initialData, action) {
 	switch (action.type) {
+		case LOADING:
+			return { ...state, status: 'pending' }
+
 		case GET_ALL_BUSINESSES:
-			return { ...state, businessArr: action.payload }
+			return { ...state, status: 'success', businessArr: action.payload }
 
 		default:
 			return state
