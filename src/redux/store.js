393909c2cb36f381c5ef4e18af59033c9a0a0af3
EdieAprovita <edieveg316@gmail.com>
2021-02-02 14:27:04 -0600

    Adding auth reducers to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,27 @@ import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 
 import recipesReducer from './recipesDucks'
-import authReducer from './authDucks'
+import {
+	userLoginReducer,
+	userRegisterReducer,
+	userDetailsReducer,
+	userUpdateProfileReducer,
+	userListReducer,
+	userDeleteReducer,
+	userUpdateReducer,
+} from './authDucks'
 import marketsReducer from './marketsDucks'
 import businessesReducer from './businessesDucks'
 import restaurantsReducer from './restaurantsDucks'
 
 const rootReducer = combineReducers({
+	userLogin: userLoginReducer,
+	userRegister: userRegisterReducer,
+	userDetails: userDetailsReducer,
+	userUpdateProfile: userUpdateProfileReducer,
+	userList: userListReducer,
+	userDelete: userDeleteReducer,
+	userUpdate: userUpdateReducer,
 	recipes: recipesReducer,
 	markets: marketsReducer,
 	businesses: businessesReducer,
