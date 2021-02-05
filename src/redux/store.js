692c3c025fedf2b9ecd8329685d99acf4408b80c
EdieAprovita <edieveg316@gmail.com>
2021-02-05 14:44:59 -0600

    store

--- src/redux/store.js
+++ src/redux/store.js
@@ -93,9 +93,21 @@ const rootReducer = combineReducers({
 	restaurantReview: restaurantReviewCreateReducer,
 })
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+	? JSON.parse(localStorage.getItem('userInfo'))
+	: null
+
+const initialState = {
+	userLogin: { userInfo: userInfoFromStorage },
+}
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 export default function generateStore() {
-	const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+	const store = createStore(
+		initialState,
+		rootReducer,
+		composeEnhancers(applyMiddleware(thunk))
+	)
 	return store
 }
