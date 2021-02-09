798aa6f1feb49060d4f75c51429fc5aabaf176c2
EdieAprovita <edieveg316@gmail.com>
2021-02-09 11:13:57 -0600

    adding route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { Route, Switch } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 
 import Navbar from './components/Navbar'
+import Recipes from './pages/recipes/Recipes'
+import Markets from './pages/markets/Markets'
+import Restaurants from './pages/restaurant/Restaurants'
+import Businesses from './pages/businesses/Businesses'
 
 import { Provider } from 'react-redux'
 import generateStore from './redux/store'
@@ -14,7 +18,10 @@ function App() {
 			<Navbar />
 			<Container>
 				<Switch>
-					<h1>Hola mundo</h1>
+					<Route path='/recipes' component={Recipes} />
+					<Route path='/restaurants' component={Restaurants} />
+					<Route path='/markets' component={Markets} />
+					<Route path='/businesses' component={Businesses} />
 				</Switch>
 			</Container>
 		</Provider>
