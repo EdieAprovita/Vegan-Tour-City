acb297c68982721d9788a0a7bc19fcd547b25cb3
Edgar Chavero <eacm316@gmail.com>
2021-05-25 15:33:51 -0500

    Cleaning main page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import { Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 
 import Navbar from './components/Navbar'
-import Recipes from './pages/recipes/RecipePage'
-import Markets from './pages/markets/MarketPage'
-import Restaurants from './pages/restaurant/RestaurantPage'
-import Businesses from './pages/businesses/BusinessPage'
-import Doctor from './pages/doctors/DoctorPage'
+
 import Footer from './components/Footer'
 
 import { Provider } from 'react-redux'
@@ -19,11 +15,7 @@ const App = () => {
 			<Navbar />
 			<main className='py-3'>
 				<Container>
-					<Route path='/recetas' component={Recipes} />
-					<Route path='/restaurantes' component={Restaurants} />
-					<Route path='/mercados' component={Markets} />
-					<Route path='/negocios' component={Businesses} />
-					<Route path='/doctores' component={Doctor} />
+					<h1>Main Page</h1>
 				</Container>
 			</main>
 			<Footer />
